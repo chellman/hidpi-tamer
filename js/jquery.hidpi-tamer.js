bf4f542e145f036fb2e9fc7652b393a61a3b0fd5
Joe Chellman <joe@shooflydesign.org>
2014-06-10 19:58:06 -0700

    Add options to the plugin

--- js/jquery.hidpi-tamer.js
+++ js/jquery.hidpi-tamer.js
@@ -16,7 +16,21 @@
 "use strict";
 
 jQuery.fn.extend({
-  hiDPITamer: function() {
+  hiDPITamer: function(options) {
+  
+  	// default value for opts
+  	// not strictly necessarily here - jQuery.extend is smart enough to deal with undefined
+  	options = options || {};
+  	
+  	var defaultOptions = {
+  		'namePattern' : /(\-|_)2x\./,
+  		'class' : 'retina',
+  	};
+  	
+  	// merge the default options for those not provided
+  	options = $.extend(defaultOptions, options);
+		// console.log(options);
+  
   	var imgObj = new Image();
   
     return this.each(function() {
@@ -30,7 +44,7 @@ jQuery.fn.extend({
       $(container).find('img').each(function(i, el) {
 				var $img = $(el);
 
-				if (/\-|_2x\./.test(el.src) || $img.hasClass('retina')) {
+				if (options.namePattern.test(el.src) || $img.hasClass(options.class)) {
 		
 					imgObj.src = el.src;
 
