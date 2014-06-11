977a48fd0a6769d1dcea0d4347c8c5cb9a0b3bde
Joe Chellman <joe@shooflydesign.org>
2014-06-10 20:23:30 -0700

    Stop hardcoding the img element.

--- js/jquery.hidpi-tamer.js
+++ js/jquery.hidpi-tamer.js
@@ -26,6 +26,7 @@
   	var defaultOptions = {
   		'namePattern' : /(\-|_)2x\./,
   		'class' : 'retina',
+  		'element' : 'img'
   	};
   	
   	// merge the default options for those not provided
@@ -34,32 +35,23 @@
   
   	var imgObj = new Image();
   
-    return this.each(function() {
-    	// I like to keep track of what "this" is
-      var container = this;
-      
-      // what is this, anyway?
-      // console.log(container);
-      // console.log($(container));
-      
-      $(container).find('img').each(function(i, el) {
-				var $img = $(el);
+    return this.find(options.element).each(function(i, el) {
+			var $img = $(el);
 
-				if (options.namePattern.test(el.src) || $img.hasClass(options.class)) {
-		
-					imgObj.src = el.src;
+			if (options.namePattern.test(el.src) || $img.hasClass(options.class)) {
+	
+				imgObj.src = el.src;
 
-					var width = $img.width(),
-							newWidth = imgObj.width / 2;
+				var width = $img.width(),
+						newWidth = imgObj.width / 2;
 
-					// console.log('width: ' + width + ' and newWidth: ' + newWidth);
+				// console.log('width: ' + width + ' and newWidth: ' + newWidth);
 
-					if (width > newWidth) {
-						$img.css('width', newWidth + 'px');
-					}
+				if (width > newWidth) {
+					$img.css('width', newWidth + 'px');
 				}
-			});
-    });
+			}
+		});
   };
  
 }( jQuery ));
